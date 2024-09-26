d4e324d4d0de5771e6daeaf781cc29d8b5b5c34b
Atanas <atodorov98@gmail.com>
2024-09-26 12:21:51 +0300

    add button component

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+export const Button = ({ text, icon, url }) => {
+  return (
+    <a href={url}>
+      <button
+        type="button"
+        class="text-green-900 bg-gray-50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2 border-2"
+      >
+        {icon}
+        {text}
+      </button>
+    </a>
+  );
+};
